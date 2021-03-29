87dc4e3111108c3d1a0133e7a5681a05d18215b1
zoldycc <sanjheevisv@gmail.com>
2021-03-29 15:23:00 +0530

    raphael: palladium bringup
    Signed-off-by: zoldycc <sanjheevisv@gmail.com>

--- palladium_raphael.mk
+++ palladium_raphael.mk
@@ -6,16 +6,18 @@
 
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Palladium stuff.
+$(call inherit-product, vendor/palladium/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := raphael
 PRODUCT_MODEL := Mi 9T Pro
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_raphael
+PRODUCT_NAME := palladium_raphael
 
 BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_BOOT_ANIMATION_RES := 1080
+PALLADIUM_BUILDTYPE := OFFICIAL
