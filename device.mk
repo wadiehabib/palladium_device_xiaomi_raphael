96a0cd014290fc8df023233b19e89443ac12e88a
mans0amador <stardroid.sy@gmail.com>
2021-09-06 20:33:36 +0300

    Revert "raphael: Implement SunlightEnhancement LiveDisplay HAL"
    This reverts commit 04d043a10ad91eafe1f57e8e57afbe58747f3843.

--- device.mk
+++ device.mk
@@ -283,11 +283,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_raphael
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.1-service.raphael
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
