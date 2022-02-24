90bc3d7b0fabba6a2a773651bb1dad6d1ae4f5ab
IPSBHANGU <inderpreetbhangu2@gmail.com>
2022-02-24 16:37:39 +0100

    milahaina: build Build mock hidl power stats  ** resolves hwservicemanager: getTransport: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device VINTF manifest. log spamming

--- device.mk
+++ device.mk
@@ -314,7 +314,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
