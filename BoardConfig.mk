02d839c3e6cc0e913b0ec431c31df3a4f4a9fd8b
IPSBHANGU <inderpreetbhangu2@gmail.com>
2022-03-09 08:42:44 +0100

    milahaina: BoardConfig: WIP set SELINUX_IGNORE_NEVERALLOWS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,6 +184,7 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 BOARD_VNDK_VERSION := current
