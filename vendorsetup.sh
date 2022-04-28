36ace281d79fa5bbf5ea6845bcc56f1b343d0dc9
wadiehabib <wadie4jesus@gmail.com>
2022-04-28 08:40:58 +0000

    pal: use vendorsetup script to clone HALs

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+rm -rf hardware/qcom-caf/sm8150
+git clone https://github.com/wadiehabib/hardware_qcom-caf_sm8150_display.git -b 12_soviet hardware/qcom-caf/sm8150/display
+git clone https://github.com/yaap/hardware_qcom-caf_sm8150_audio -b twelve hardware/qcom-caf/sm8150/audio
+git clone https://github.com/yaap/hardware_qcom-caf_sm8150_media -b twelve hardware/qcom-caf/sm8150/media
