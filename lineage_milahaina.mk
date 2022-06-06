5b533dae1239c2e2011388712b01b7a9d4004339
IPSBHANGU <inderpreetbhangu2@gmail.com>
2022-06-06 17:19:32 +0200

    milahaina: update build prop configs

--- lineage_milahaina.mk
+++ lineage_milahaina.mk
@@ -17,13 +17,13 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_BRAND := xiaomi
 PRODUCT_DEVICE := milahaina
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := Mi 11
+PRODUCT_MODEL := Mi 11T Pro
 PRODUCT_NAME := lineage_milahaina
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=milahaina \
-    PRODUCT_NAME=HAYDN
+    PRODUCT_NAME=vili
 
-BUILD_FINGERPRINT := Xiaomi/haydn_in/haydnin:11/RKQ1.201112.002/V12.0.3.0.RKKCNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/vili/vili:11/RKQ1.210503.001/V12.5.2.0.RKDINXM:user/release-keys
